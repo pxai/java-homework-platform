60313fd0a6c9296fa145d77f5fcbe43080848ffb
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-11-29 12:45:12 +0100

    first commit

---
+++ src/main/java/io/pello/homework/javahomeworkplatform/JavaHomeworkPlatformApplication.java
@@ -0,0 +1,12 @@
+package io.pello.homework.javahomeworkplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHomeworkPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHomeworkPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/io/pello/homework/javahomeworkplatform/JavaHomeworkPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pello.homework.javahomeworkplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaHomeworkPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
